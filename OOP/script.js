4d8aeb2f27126ece7ec2aea2eea7088f94c4c46a
0-L-G-A <lol79lol97@gmail.com>
2021-09-07 02:56:29 +0300

    OOP

--- OOP/script.js
+++ OOP/script.js
@@ -1,81 +1,81 @@
-// console.log('TASK 1');
+console.log('TASK 1');
 
-// class Student1{
-//     constructor(fullName, direction){
-//         this.fullName = fullName;
-//         this.direction = direction;
-//     }
-//     showFullName(){return this.fullName};
-//     nameIncludes(data){
-//         return this.fullName.includes(data);
-//     }
-//     static studentBuilder(fullN, dir){
-//         return new Student1(fullN, dir);
-//     }
-//     get direction(){
-//         return this._direction;
-//     }
-//     set direction(newD){
-//         this._direction = newD;
-//     }
-// }
+class Student1{
+    constructor(fullName, direction){
+        this.fullName = fullName;
+        this.direction = direction;
+    }
+    showFullName(){return this.fullName};
+    nameIncludes(data){
+        return this.fullName.includes(data);
+    }
+    static studentBuilder(fullN, dir){
+        return new Student1(fullN, dir);
+    }
+    get direction(){
+        return this._direction;
+    }
+    set direction(newD){
+        this._direction = newD;
+    }
+}
 
-// const stud1 = new Student1('Ivan Petrenko', 'web');
-// const stud2 = new Student1('Sergiy Koval', 'python');
-// const stud3 = Student1.studentBuilder('Ihor Kohut', 'qc');
-// console.log(stud1);
-// console.log(stud2);
-// console.log(stud3);
+const stud1 = new Student1('Ivan Petrenko', 'web');
+const stud2 = new Student1('Sergiy Koval', 'python');
+const stud3 = Student1.studentBuilder('Ihor Kohut', 'qc');
+console.log(stud1);
+console.log(stud2);
+console.log(stud3);
 
-// console.log('Full name');
-// console.log(stud1.showFullName());
-// console.log('Name includes: Ivan');
-// console.log(stud1.nameIncludes('Ivan'));
-// console.log('Name includes: b');
-// console.log(stud1.nameIncludes('b'));
-// console.log('Change direction of second student:');
-// console.log(stud2.direction);
-// stud2.direction = 'ui';
-// console.log(stud2.direction);
-
-
+console.log('Full name');
+console.log(stud1.showFullName());
+console.log('Name includes: Ivan');
+console.log(stud1.nameIncludes('Ivan'));
+console.log('Name includes: b');
+console.log(stud1.nameIncludes('b'));
+console.log('Change direction of second student:');
+console.log(stud2.direction);
+stud2.direction = 'ui';
+console.log(stud2.direction);
 
 
-// console.log(' ');
-// console.log('TASK 2');
 
-// class Person{
-//     constructor(firstName, lastName){
-//         this.firstName = firstName;
-//         this.lastName = lastName;
-//     }
-//     showFullName(){
-//         return this.firstName + " " + this.lastName
-//     };
-// }
 
-// class Student extends Person{
-//     constructor(firstName, lastName, year){
-//         super(firstName, lastName);
-//         this.year = year;
-//     }
-//     showFullName(middleName){
-//         return this.firstName + " " + this.lastName + " " + middleName
-//     };
-//     showCourse(){
-//         let today = new Date();
-//         let currentYear = today.getFullYear();
-//         const course = currentYear - this.year;
-//         if(course <= 6){return course}
-//         if(course > 6){return `Student finished the university ${course - 6} ago`}
-//     }
-// }
+console.log(' ');
+console.log('TASK 2');
 
-// const s1 = new Student("Petro", "Petrenko", 2017);
-// console.log(s1);
-// console.log('Full Name:');
-// console.log(s1.showFullName("Petrovych")); // Petrenko Petro Petrovych
-// console.log("Current course: " + s1.showCourse()); //Current course: 4
+class Person{
+    constructor(firstName, lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    showFullName(){
+        return this.firstName + " " + this.lastName
+    };
+}
+
+class Student extends Person{
+    constructor(firstName, lastName, year){
+        super(firstName, lastName);
+        this.year = year;
+    }
+    showFullName(middleName){
+        return this.firstName + " " + this.lastName + " " + middleName
+    };
+    showCourse(){
+        let today = new Date();
+        let currentYear = today.getFullYear();
+        const course = currentYear - this.year;
+        if(course <= 6){return course}
+        if(course > 6){return `Student finished the university ${course - 6} ago`}
+    }
+}
+
+const s1 = new Student("Petro", "Petrenko", 2017);
+console.log(s1);
+console.log('Full Name:');
+console.log(s1.showFullName("Petrovych")); // Petrenko Petro Petrovych
+console.log("Current course: " + s1.showCourse()); //Current course: 4
 
 
 
@@ -84,31 +84,34 @@ console.log(' ');
 console.log('TASK 3');
 
 class Worker{
+    #experience = 1.2;
+    #salary = this.dayRate * this.workingDays * this.#experience;
     constructor(fullName, dayRate, workingDays){
         this.fullName = fullName;
         this. dayRate = dayRate;
         this.workingDays = workingDays;
-        this.experience = 1.2;
-        this.salary = this.dayRate * this.workingDays * this.experience;
     }
     showSalary(){
         return this.dayRate * this.workingDays;
     }
     get showExp(){
-        return this.experience;
+        return this.#experience;
     }
     set setExp(val){
-        this.experience = val;
+        this.#experience = val;
     }
     showSalaryWithExperience(){
-        return this.dayRate * this.workingDays * this.experience;
+        const salary = this.dayRate * this.workingDays * this.#experience;
+        this.#salary = salary;
+        return salary;
     }
     static sortSalaries(...workers){
-        const sortedArr = workers.sort((a,b) => a.salary - b.salary)
-        let entries = sortedArr.map((worker) => [(worker.fullName).replace(' ','_'), worker.salary])
+        const sortedArr = workers.sort((a,b) => a.#salary - b.#salary)
+        let entries = sortedArr.map((worker) => [(worker.fullName).replace(' ','_'), worker.#salary])
         return Object.fromEntries(entries);
     }
 }
+
 
 const worker1 = new Worker("John Johnson", 20, 23);
 console.log(worker1);
